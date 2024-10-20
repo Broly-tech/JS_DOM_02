a384bd53fbc37e563160225befa65a3d26f4c036
Broly-tech <anuranchowdhury.ac@gmail.com>
2024-10-20 22:05:48 +0530

    Sample Project 01

---
+++ script.js
@@ -0,0 +1,14 @@
+let divs=document.querySelector(".box");
+
+console.log(divs)
+/*let divs=document.querySelectorAll(".box");
+console.log(divs)
+/*divs[0].innerText="First Unique Value";
+divs[1].innerText="Second Unique Value";
+divs[2].innerText="Third Unique Value";*/
+/*let index=1
+for(div of divs)
+{
+ div.innerText=`Unique value ${index}`
+ index++;
+}*/
