7308d2d01fe143719815f467a7cd41c4bd08f077
Broly-tech <anuranchowdhury.ac@gmail.com>
2024-10-22 12:08:32 +0530

    Add files via upload

---
+++ Javascript_Assignment_03/script.js
@@ -0,0 +1,7 @@
+//Create a p tag in html.Give it a class and some styling 
+//Create a new class and append this class to the p element
+
+const para=document.querySelector("p");
+para.classList.add("newClass");
+
+
